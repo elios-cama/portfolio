cf75b09ec8ef0714a3cc5c86de341eb7008b0242
Elios Cama <elios@daymode.fr>
2023-07-22 11:01:51 +0200

    changed the design of the side bar and of the theme icon

--- lib/pages/side_bar/side_bar.dart
+++ lib/pages/side_bar/side_bar.dart
@@ -1,7 +1,8 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:portflolio/pages/side_bar/side_bar_provider.dart';
-import 'package:portflolio/theme/constants.dart';
 import '../../main.dart';
 import '../../widgets/sideBar/drawer_list_tile.dart';
 
@@ -14,17 +15,28 @@ class SideBar extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final pageIndex = ref.watch(sideBarIndexProvider);
     return Drawer(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(0),
+          bottomRight: Radius.circular(0),
+        ),
+      ),
       backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       child: ListView(
         children: [
-          DrawerHeader(
-            child: CircleAvatar(radius: (80),
-                backgroundColor: Colors.transparent,
-                child: ClipRRect(
-                  borderRadius:BorderRadius.circular(18),
-                  child: Image.asset("assets/images/elios.png", fit: BoxFit.fill,),
-                )
-            )
+          Padding(
+            padding: const EdgeInsets.all(18),
+            child: CircleAvatar(
+              radius: (80),
+              backgroundColor: Colors.transparent,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(18),
+                child: Image.asset(
+                  "assets/images/elios.png",
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
           ),
           const DrawerListTile(
             svgName: 'profile',
@@ -58,18 +70,29 @@ class SideBar extends ConsumerWidget {
           ),
           Consumer(builder: (context, ref, child) {
             final theme = ref.watch(themeModeProvider);
-            return IconButton(
-                onPressed: () {
-                  ref.read(themeModeProvider.notifier).state =
-                  theme == ThemeMode.light
-                      ? ThemeMode.dark
-                      : ThemeMode.light;
-                },
-                icon: Icon(theme == ThemeMode.dark
-                    ? Icons.light_mode
-                    : Icons.dark_mode,color: theme == ThemeMode.dark
-                    ? Theme.of(context).colorScheme.primary
-                    : Theme.of(context).colorScheme.primary,));
+            return ListTile(
+              onTap: () {
+                ref.read(themeModeProvider.notifier).state =
+                    theme == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+              },
+              horizontalTitleGap: 0.0,
+              leading: SvgPicture.asset(
+                theme == ThemeMode.dark ? 'assets/svg/leaf.svg' : 'assets/svg/drop.svg',
+                colorFilter: ColorFilter.mode(
+                    theme == ThemeMode.dark
+                        ? Theme.of(context).colorScheme.primary
+                        : Theme.of(context).colorScheme.primary,
+                    BlendMode.srcIn),
+                height: 16,
+                width: 16,
+                fit: BoxFit.scaleDown,
+              ),
+              title: AutoSizeText(
+                "Changer de thème",
+                maxLines: 2,
+                style: TextStyle(color: Theme.of(context).colorScheme.primary, fontSize: 18),
+              ),
+            );
           }),
         ],
       ),
