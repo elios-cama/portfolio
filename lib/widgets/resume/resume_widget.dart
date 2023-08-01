21e2fcf59188aac51140431f4a7071dbcb2068c9
elios cama <elios.cama@gmail.com>
2023-08-01 10:01:31 +0200

    added the resume button and deleted macos and linux files

--- lib/widgets/resume/resume_widget.dart
+++ lib/widgets/resume/resume_widget.dart
@@ -11,51 +11,65 @@ class ResumeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => router.go('/resume'),
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(24),
-          color: Theme.of(context).colorScheme.primary,
-        ),
-        child: const Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              flex: 1,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  AutoSizeText(
-                    "A propos",
-                    style: TextStyle(color: Colors.white, fontFamily: 'Archivo', fontSize: 36),
-                    overflow: TextOverflow.fade,
-                    textAlign: TextAlign.center,
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(24),
+        color: Theme.of(context).colorScheme.primary,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Expanded(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AutoSizeText(
+                  "A propos",
+                  style: TextStyle(
+                      color: Colors.white, fontFamily: 'Archivo', fontSize: 36),
+                  overflow: TextOverflow.fade,
+                  textAlign: TextAlign.center,
+                ),
+                AutoSizeText(
+                  "CV",
+                  style: TextStyle(
+                      color: Colors.white, fontFamily: 'Archivo', fontSize: 36),
+                  overflow: TextOverflow.fade,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+          const Spacer(),
+          const Expanded(
+            flex: 7,
+            child: AutoSizeText(
+              "Bonjour ! J'ai 22 ans et je suis un étudiant passioné par les nouvelles technologies. Je suis actuellement en Erasmus à Polytechnique Milan et recherche un stage de fin d'études pour le printemps 2024. J'aimerai allier mes connaissances en développement mobile, modélisation 3D et en IA.",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+              maxLines: 10, // Adjust this value based on your requirement
+              overflow: TextOverflow.fade,
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  AutoSizeText(
-                    "CV",
-                    style: TextStyle(color: Colors.white, fontFamily: 'Archivo', fontSize: 36),
-                    overflow: TextOverflow.fade,
-                    textAlign: TextAlign.center,
-                  ),
-                ],
+                ),
+                onPressed: () => router.go('/resume'),
+                child: const Text("Cliquer ici pour voir mon CV"),
               ),
             ),
-            Spacer(),
-            Expanded(
-              flex: 7,
-              child: AutoSizeText(
-                "Bonjour ! J'ai 22 ans et je suis un étudiant passioné par les nouvelles technologies. Je suis actuellement en Erasmus à Polytechnique Milan et recherche un stage de fin d'études pour le printemps 2024. J'aimerai allier mes connaissances en développement mobile, modélisation 3D et en IA.\nCliquer sur la case pour voir mon CV",
-                style: TextStyle(color: Colors.white, fontSize: 20),
-                maxLines: 10, // Adjust this value based on your requirement
-                overflow: TextOverflow.fade,
-                textAlign: TextAlign.left,
-              ),
-            ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
